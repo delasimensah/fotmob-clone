9d180f1f3e980603243f986be0f6a875d1d717af
delasimensah <mensadelasi@gmail.com>
2022-12-22 09:59:46 +0000

    made some minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="font-semibold">
-        Whereas recognition of the inherent dignity
-      </h1>
-    </>
-  );
-}
+const Home = () => {
+  return <h1 className="font-semibold">Fotmob Clone</h1>;
+};
+
+export default Home;
